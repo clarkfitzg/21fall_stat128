f3ff76efd3f9f94d716cde1d9a578fbc346bd027
Clark Fitzgerald <clarkfitzg@gmail.com>
2021-09-02 15:22:35 -0700

    class

---
+++ 2_files_df_vectors_class.R
@@ -0,0 +1,36 @@
+# R script
+
+# Working directories
+
+
+# YOUR TURN
+# Are absolute paths good for sharing?
+
+# Shortcuts:
+# ~   home directory
+# ..  parent directory
+
+# list files in a directory
+
+# Download data file
+# https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-23/earn.csv
+
+
+# 1.7 Vectors
+############################################################
+
+
+
+
+
+# YOUR TURN:
+# Determine which of the following functions work elementwise.
+# log, mean, sum, sqrt
+
+
+# If two vectorized arguments are not the same size, then R will
+# *recycle* the smaller to be the same size.
+
+
+# Comparisons
+
