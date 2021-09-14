7a31c36edaacd3ef91e0f0249f8161844288394b
Clark Fitzgerald <clarkfitzg@gmail.com>
2021-09-14 10:29:41 -0700

    .

--- 5_indexing.R
+++ 5_indexing.R
@@ -6,6 +6,7 @@
 # Outcomes:
 #   
 # 1. Use factors to represent categorical variables.
+# 2. Index elements from data structures
 
 # Factors represent categorical variables.
 ############################################################
@@ -54,5 +55,71 @@ as.integer(as.character(year))
 
 x = c(a = 10, b = 20, c = 30, d = 40, e = 50)
 
+# In math, we often write a vector of length n as:
+# [x_1, x_2, x_3, ..., x_n]
+
 # Q: What should this do?
 x[1]
+
+# Q: What will x look like after this reassignment?
+x[1] = 1000
+
+# Q: What if I index x by the numbers from 1 to 3?
+x[1:3]
+x[1:3] = c(4, 8, 16)
+
+# Q: The object on the left hand side of the assignment is a vector with 3 elements.
+#   The object on the right is a vector of length 1.
+#   What's gonna happen?!
+x[1:3] = NA
+
+x[c(1, 4)]
+pos = c(1, 5)
+x[pos]
+
+# Q: If POSITION is a vector of integers, what does X[POSITION] return?
+
+# Q: Does above the rule hold up if the indices are repeated?
+x = c(a = 10, b = 20, c = 30, d = 40, e = 50)
+x[c(1, 4, 1, 2)]
+
+# Q: What if you want all elements of x EXCEPT position 2?
+x[-2]
+
+x[c(-2, -4)]
+
+
+# Indexing by name
+############################################################
+
+x = c(a = 10, b = 20, c = 30, d = 40, e = 50)
+
+x["b"]
+
+# Q: Make a prediction, what will these do, and why?
+x["a"]
+x[a]
+
+# Indexing by condition
+############################################################
+# The most powerful way to index
+
+lessthan25 = x < 25   #  example of an __intermediate variable__
+
+x[lessthan25]
+
+# Q: Does indexing do the same thing regardless of whether I use 
+#   an intermediate variable?
+x[x < 25]
+
+
+x_less_than25 = x[x < 25]
+
+# Q: Does this make any sense?
+x + x_less_than25
+
+
+
+# Q: Is it better to use integers or meaningful names?
+
+
