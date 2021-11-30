4757c074a9cbb2c4d0a40cdf667729aead821bda
Clark Fitzgerald <clarkfitzg@gmail.com>
2021-11-29 16:16:31 -0800

    .

---
+++ 23_bootstrap.R
@@ -0,0 +1,27 @@
+# Announcements:
+#
+# 1. Project 2 drafts due tomorrow (Dec 1) for feedback
+# 2. Sign up for a presentation slot next week
+#
+# Outcomes:
+#
+# 1. Apply the naive bootstrap to create confidence intervals for univariate statistics
+#
+# Reference: https://en.wikipedia.org/wiki/Bootstrapping_(statistics)
+
+# There are MANY variations of bootstrap and resampling methods.
+# Our goal today is just the most basic one.
+
+
+# Like most statistical methods, the bootstrap relies on IID data.
+# What does IID mean?
+
+# The central limit theorem says ...
+
+# Simple bootstrap procedure:
+
+# How can we get a confidence interval for the 1st and 3rd quartiles?
+
+# Why is it called the naive bootstrap?
+
+# bootstrapping residuals (if we have time)
